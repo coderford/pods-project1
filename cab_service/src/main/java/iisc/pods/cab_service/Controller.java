7cd58808b5208b869c39942019350b2ce8765c2d
Aditya Kumar <kumar.sava.aditya@gmail.com>
2021-03-24 00:02:38 +0530

    Initialize Cab service along with stubs for endpoints

---
+++ cab_service/src/main/java/iisc/pods/cab_service/Controller.java
@@ -0,0 +1,53 @@
+package iisc.pods.cab_service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @RequestMapping("/")
+    public String home() {
+        return "Hello";
+    }
+
+    @RequestMapping("/requestRide")
+    public boolean requestRide(
+        @RequestParam int cabId,
+        @RequestParam int rideId,
+        @RequestParam int sourceLoc,
+        @RequestParam int destinationLoc
+    ) {
+        return true;
+    }
+
+    @RequestMapping("/rideStarted")
+    public boolean rideStarted(@RequestParam int cabId, @RequestParam int rideId) {
+        return true;
+    }
+
+    @RequestMapping("/rideCanceled")
+    public boolean rideCanceled(@RequestParam int cabId, @RequestParam int rideId) {
+        return true;
+    }
+
+    @RequestMapping("/rideEnded")
+    public boolean rideEnded(@RequestParam int cabId, @RequestParam int rideId) {
+        return true;
+    }
+
+    @RequestMapping("/signIn")
+    public boolean signIn(@RequestParam int cabId, @RequestParam int initialPos) {
+        return true;
+    }
+
+    @RequestMapping("/signOut")
+    public boolean signOut(@RequestParam int cabId) {
+        return true;
+    }
+
+    @RequestMapping("/numRides")
+    public boolean numRides(@RequestParam int cabId) {
+        return true;
+    }
+}
