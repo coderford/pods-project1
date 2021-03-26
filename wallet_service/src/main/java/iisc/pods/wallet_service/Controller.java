7f049b3669c7e23a68151ece444d86ffffa323dc
Aditya Kumar <kumar.sava.aditya@gmail.com>
2021-03-26 23:24:07 +0530

    Initialize wallet service and add stubs for endpoints

---
+++ wallet_service/src/main/java/iisc/pods/wallet_service/Controller.java
@@ -0,0 +1,29 @@
+package iisc.pods.wallet_service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/getBalance")
+    public int getBalance(@RequestParam int custId) {
+        return 0;
+    }
+
+    @RequestMapping("/deductAmount")
+    public boolean deductAmount(@RequestParam int custId, @RequestParam int amount) {
+        return true;
+    }
+
+    @RequestMapping("/addAmount")
+    public boolean addAmount(@RequestParam int custId, @RequestParam int amount) {
+        return true;
+    }
+
+    public void reset() {
+
+    }
+    
+}
