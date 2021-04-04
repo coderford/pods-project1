f003192559ca79873ad1bb6727dfc68c8ff940f9
Ashish Bokil <ashishbkl11@gmail.com>
2021-04-04 19:20:46 +0530

    created database

---
+++ ride_service/src/main/java/com/CabCompany/RideService/CustRepo.java
@@ -0,0 +1,10 @@
+package com.CabCompany.RideService;
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustRepo extends CrudRepository<Customer,Integer>{
+    
+}
